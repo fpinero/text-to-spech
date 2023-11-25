dbd160e3d49cf3a660ae4787ef4e385fec144694
Fernando Piñero Estrada <Fernando.Pinero.ext@bizerba.com>
2023-11-25 11:50:00 +0100

    added feaure that allows you to choose different voices

--- docx-to-mp3.py
+++ docx-to-mp3.py
@@ -11,52 +11,49 @@ def read_docx(file_path):
     return ' '.join(full_text)
 
 
+def choose_voice_and_rate():
+    voices = {
+        'a': ('Lucia', '85%'),
+        'b': ('Conchita', '99%'),
+        'c': ('Enrique', '92%')
+    }
+
+    print("Please choose a voice:")
+    for key, value in voices.items():
+        print(f"{key}) {value[0]}")
+
+    choice = input("Enter your choice (a, b, or c): ").lower()
+    return voices.get(choice, ('Lucia', '85%'))  # Devuelve 'Lucia' y '85%' como valores predeterminados
+
+
 def convert_docx_to_mp3():
-    # get the file path from the user
     file_path = input('Please enter the path to the Word document or type "exit" to abort: ')
 
-    # check if the user wants to exit
     if file_path.lower() == 'exit':
         print('Program aborted by the user.')
-        sys.exit()  # Termina la ejecución del programa
+        sys.exit()
 
-    # read the contents of the docx file
+    voice_id, rate = choose_voice_and_rate()
+
     text = read_docx(file_path)
-
-    # rename the file path to a .mp3
     mp3_output = file_path.rsplit('.', 1)[0] + '.mp3'
-
-    # create boto3 client for polly
     polly_client = boto3.Session(region_name='eu-central-1').client('polly')
-
-    # maximum length for a synthesize_speech call
     max_chunk_len = 1000
-
-    # split input text into chunks
     chunks = [text[i:i + max_chunk_len] for i in range(0, len(text), max_chunk_len)]
-
-    # initialize an empty byte array to hold audio stream
     stream = bytearray()
 
-    # synthesize speech for each chunk and add to the stream
     for i, chunk in enumerate(chunks, start=1):
-        # add prosody tag to slow down the speech rate to 85% of the original
-        ssml_text = f'<speak><prosody rate="85%">{chunk}</prosody></speak>'
-        response = polly_client.synthesize_speech(VoiceId='Lucia',
+        ssml_text = f'<speak><prosody rate="{rate}">{chunk}</prosody></speak>'
+        response = polly_client.synthesize_speech(VoiceId=voice_id,
                                                   OutputFormat='mp3',
                                                   TextType='ssml',
                                                   Text=ssml_text)
-        # add the current audio chunk to the stream
         stream += response['AudioStream'].read()
-
-        # Print the progress
         sys.stdout.write(f'\rProcessing chunk {i}/{len(chunks)}...')
-        sys.stdout.flush()  # Asegúrate de que el mensaje se imprima inmediatamente
+        sys.stdout.flush()
 
-    # Print a newline character to ensure the next print statement is on a new line
     sys.stdout.write('\n')
 
-    # save the audio stream to a file
     with open(mp3_output, 'wb') as f:
         f.write(stream)
 
